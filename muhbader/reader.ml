9d7207b0b3d2462369c1fbd1f300a6c249a6be6e
muhammad <xpz6500@gmail.com>
2020-11-18 19:25:56 +0200

    bug fixed

--- muhbader/reader.ml
+++ muhbader/reader.ml
@@ -1,5 +1,3 @@
-(* We assert that the work we submitted is 100% our own. We have not received any part from any other student in the class, nor have we give parts of it for use to others. Nor have we used code from other sources: Courses taught previously at this university, courses taught at other universities, various bits of code found on the Internet, etc. *)
-(* We realize that should our code be found to contain code from other sources, that a formal case shall be opened against us with va’adat mishma’at, in pursuit of disciplinary action. *)
 #use "pc.ml";;
 open PC;;
 exception X_not_yet_implemented;;
@@ -29,7 +27,6 @@ let rec sexpr_eq s1 s2 =
   | Symbol(s1), Symbol(s2) -> s1 = s2
   | Pair(car1, cdr1), Pair(car2, cdr2) -> (sexpr_eq car1 car2) && (sexpr_eq cdr1 cdr2)
   |_ -> raise X_no_match;;
-
   
 module Reader: sig
   val read_sexprs : string -> sexpr list
@@ -70,8 +67,6 @@ let get_option some_val =
     | Some a -> a
     | None -> None;;
 
-(* -------------------------------------------------------------------------------------------------------- *)
-
 let string_metachar
   = caten (char ('\\')) (const (fun ch -> ch='f'||ch='n'||ch='\\'||ch='t'||ch='r'||ch='"'));;
 
@@ -85,7 +80,6 @@ let replace_metachar s =
     | ('\\', '\"') -> '\"'
     | (s, r) -> raise X_no_match;;
 
-
 let stringLiteralChar =  (const (fun ch -> ch!='\"' && ch!= '\\'));;
 
 let strignChar
@@ -96,8 +90,9 @@ let rec gcd a b =
 
 let do_gcd a b = 
   let x = gcd a b in
-    (a/x, b/x);;
-
+  if x>0 then
+    (a/x, b/x)
+    else (a/(-1*x), b/(-1*x));;
 
 let tenEx num ex = 
   let rec pow a = function
@@ -108,12 +103,6 @@ let tenEx num ex =
       b *. b *. (if n mod 2 = 0 then 1. else a) in
   let times = pow 10. ex in
   num *. times;;
-
-(* -------------------------------------------------------------------------------------------------------- *)
-
-
-
-(* let nt_numberE = pack (caten (caten (nt_number) (char_ci 'e')) (nt_integer)) (fun ((e,s),r)->e@(s::r));;  *)
 
 let nt_boolt = make_spaced (word_ci "#t");;
 
@@ -147,11 +136,6 @@ let nt_disj_nt_list l=
     l
     nt_none;;
 
-
-(* ------------------------------------------------ Compilers ----------------------------------------------- *)
-
-
-  (* ----------------  symbol -----------------*)
 let nt_specialchar = disj_l ['!';'$';'^';'*';'-';'_';'+';'=';'<';'>';'?';'/';':'] char;;
 let symNums = range '0' '9';;
 let symLetters = range_ci 'a' 'z';;
@@ -159,8 +143,6 @@ let symbolCharNoDot = disj (disj symNums symLetters) nt_specialchar;;
 let dot =  char '.';;
 let symChar = disj symbolCharNoDot dot;;
 
-
-  (* ----------------  number -----------------*)
 let natural =
   let digits =  plus digit in
   pack digits (fun (ds) -> ds);;
@@ -189,7 +171,6 @@ let exponent_float (((domi, symb), nomi), expo) = match symb with
                                 |_ -> raise X_no_match)
       |_-> raise X_no_match
                                 
-
 let number s = 
     let (((domi, symb), nomi), rest) = 
       try (fraction s)
@@ -198,7 +179,7 @@ let number s =
         with PC.X_no_match -> pack integer (fun x -> ((x, '_'), ['1'])) s
       ) 
       in
-      let (scientific, rest) = maybe (char 'e') rest in
+      let (scientific, rest) = maybe (char_ci 'e') rest in
       let (exponent, rest) = match scientific with
       |Some(e) -> let  (expo, rest) = integer rest in (['e']@expo, rest)
       |None -> (['_'], rest) in
@@ -225,43 +206,30 @@ let nt_namedChar s =
     |"space" -> (' ',s)
     |"tab" ->('\t', s)
     |e -> raise X_no_match;;
-
-
 let rec pairs lst = match lst with
     | [] -> Nil
     |first:: rst -> Pair(first, pairs rst);;
-
-
 let rec nt_expr s =
   let nt_nestedexp = pack (caten (caten tok_lparen nt_expr) tok_rparen)
       (fun ((l, e), r) -> e) in
   (disj nt_number nt_nestedexp) s
-
 and nt_string s = 
   let st = (pack (caten (caten nt_leftquotation (star  strignChar)) nt_rightquotation)
                 (fun ((l, e), r) -> String(list_to_string e))) in st s
-
-
 and nt_bool = disj (pack nt_boolt (fun _-> Bool(true))) 
   (pack nt_boolf (fun _-> Bool(false)))
-
-
 and nt_char = pack (caten (caten charPrefix (disj visiblesimplechar nt_namedChar)) nt_whitespaces) 
       (fun ((pre, vis), spaces) -> Char(vis))
-
-and nt_number =  make_spaced number
-
+and nt_number =  not_followed number (disj symLetters nt_specialchar)
 and nt_symbol =  disj (fun x ->
   let ((sym,slst), rest) = caten symChar (plus symChar) x in
   (Symbol(list_to_string (sym::slst)), rest)) 
   (fun s -> let (sym,rest) = symbolCharNoDot s in (Symbol(list_to_string [sym]), rest))
-
 and nt_list s = let p = pack 
     (caten (caten tok_lparen (star (nt_sexpr))) tok_rparen) 
       (fun ((l,exps), r) -> (List.fold_right(
                 (fun x acc  -> Pair(x ,acc)))) exps Nil)
                  in p s
-
 and nt_dotted_list s = let dotted = pack 
       (
         caten (caten tok_lparen (plus nt_sexpr)) (caten (caten (make_spaced dot) nt_sexpr) tok_rparen)
@@ -279,24 +247,15 @@ and nt_all_quotes s = let (quete,sexp) = match s with
       |_ -> raise X_no_match 
       in let (s,r) = (star nt_sexpr) sexp in 
       (Pair(Symbol(quete), pairs s), r)
-
 and nt_sexprcomment s = pack (caten (caten (word "#;") (nt_sexpr)) (maybe (nt_sexpr)))
       (fun ((s,e),r)-> match r with | None -> Nil | Some r -> r ) s
-
 and nt_comment s = let (_ ,s) = caten (char ';') (star (const (fun ch -> ch!='\n'))) s in
       match s with 
         |'\n'::rest -> nt_sexpr rest
         | _ -> (Nil, [])
-
-      (* (maybe nt_sexpr))
-      (fun ((s,e),r)-> match r with | None -> Nil | Some r -> r) s *)
-
-
 and nt_sexpr s =  let nt_l = [
   nt_number; nt_char;nt_string; nt_bool;nt_symbol;nt_list;nt_dotted_list;nt_all_quotes;nt_comment;nt_sexprcomment] in
   (make_spaced(nt_disj_nt_list nt_l)) s;;
 
 let read_sexprs string = let (sexp, lst) = plus nt_sexpr (string_to_list string) in sexp;;
-
-  
 end;; (* struct Reader *)
